0ab56565ec318207219c16a2da4642124ffe0bfa
Helder Pereira <helder.pereira@xing.com>
2020-06-16 16:09:04 +0200

    Changed to PikaPika

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import AddForm from './components/AddForm'
 import EditForm from './components/EditForm'
 
 // const url = 'http://localhost:4000/api/'
-const url = 'http://simplelistserver-env.eba-eny7zm3e.eu-central-1.elasticbeanstalk.com/api/'
+const url = 'https://pikapika.azurewebsites.net/api'
 
 const App = () => {
 
